4f1fef432f7ce8696852cc8132950ff6c63e8bb6
Refija <37211053+Refija@users.noreply.github.com>
2019-05-27 22:26:39 +0200

    Add files via upload

---
+++ dbo.Procedure_1.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[MemberRent]
+	@MemberID int
+AS
+	SELECT M.*, R.RentID, B.Title from Members as M
+	inner join Rents as R on R.MemberID=M.MemberID
+	inner join Books as B on B.BookID=R.BookID
+	where M.MemberID=@MemberID
+
+RETURN 0
